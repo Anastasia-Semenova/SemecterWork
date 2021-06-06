12b4acab93776baff6b7f667931a8eefc8f1cc06
Anastasia-Semenova <65806540+Anastasia-Semenova@users.noreply.github.com>
2021-06-06 23:46:41 +0300

    Delete Simbirsoft/Chat directory

--- Simbirsoft/Chat/src/main/java/com/simbirsoft/chat/repositories/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.simbirsoft.chat.repositories;
-
-import com.simbirsoft.chat.model.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
